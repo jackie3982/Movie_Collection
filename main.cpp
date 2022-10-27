401fb06cc2d669344ba3edaa5e86200d43948706
jackie3982 <jaccbybe@ut.utm.edu>
2022-10-27 16:02:20 -0500

    first push

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Jacqueline Bybee
+ * @date 2022-10-27
+ * @brief Driver of the movie collection program 
+ * 
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  
+  return 0;
+}
